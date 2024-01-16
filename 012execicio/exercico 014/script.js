c35b80597054f5e9cda9b48be4ce810371979346
iago correia de lima <115280746+iagob2@users.noreply.github.com>
2024-01-16 14:31:01 -0300

    exercicio 12 final

--- 012execicio/exercico 014/script.js
+++ 012execicio/exercico 014/script.js
@@ -1,1 +1,24 @@
-alert('teste');
+function carregar() {
+    var msg =  document.getElementById('msg');
+    var img = document.getElementById('imagem');
+    var data = new Date();
+    var hora = data.getHours();
+    var minutos = data.getMinutes();
+
+    msg.innerHTML=`agora são ${hora} horas e ${minutos} minutos `;
+
+    if(hora >= 0 && hora < 12) { 
+        //dia 
+        img.src = '../imagens/dia.png' ;
+        document.body.style.backgroundColor = '#fcbf54';
+    }else if (hora >= 12 && hora <=18) { 
+        //tarde 
+        img.src = '../imagens/tarde.png';
+        document.body.style.backgroundColor = '#826675';
+
+    }else { 
+        // noite 
+        img.src = '../imagens/noite.png';
+        document.body.style.backgroundColor = '#233a44';
+    }
+}
